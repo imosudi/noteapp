f0fde945b5b8c255afdd6f5f17645db8dbd9b7b8
Mosudi Isiaka <imosudi@gmail.com>
2017-12-18 00:32:30 +0100

    noteapp directory

---
+++ cgi-bin/noteapp/noteapp.py
@@ -0,0 +1,23 @@
+#import virtualenv python library directory
+import os
+import sys
+sys.path.insert(0, '/var/www/clients/client6/web28/cgi-bin/venv/lib/python2.7/site-packages')
+
+
+#import installed library
+from flask import Flask, render_template, request, url_for
+
+
+
+
+#Create application
+app = Flask(__name__)
+#bootstrap = Bootstrap(app)
+#moment = Moment(app)
+
+
+
+@app.route('/')
+def home():
+    return 'Welcome to noteapp'
+
