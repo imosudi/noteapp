aef5aa8c48ac46f9733c3c6fe9d6687f5e0ad745
Mosudi Isiaka <imosudi@gmail.com>
2017-12-18 00:48:34 +0100

    app models

--- cgi-bin/noteapp/models.py
+++ cgi-bin/noteapp/models.py
@@ -13,3 +13,42 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField, HiddenField, validators, BooleanField, PasswordField
 from wtforms.validators import Required
 
+
+
+class Note(db.Model): 
+    id = db.Column(db.Integer, primary_key=True) 
+    title = db.Column(db.String(80))
+    body = db.Column(db.Text)
+ 
+    def __init__(self, title, body): 
+        self.title = title
+        self.body = body
+
+class registrationForm(FlaskForm):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [ 
+        validators.DataRequired(), 
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+    submit = SubmitField('Complete Registeration')
+
+class RegistrationForm(db.Model):
+    __tablename__ = 'registrationforms'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(64), unique=True)
+    password = db.Column(db.String(64))
+    accept_tos = db.Column(db.Boolean)
+
+    title = db.Column(db.String(80))
+    body = db.Column(db.Text)
+
+    def __init__(self, username, email, password, accept_tos):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.accept_tos = accept_tos
+
