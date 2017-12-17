47611c3caf0350df4d32ce6a4458d92980e17a59
Mosudi Isiaka <imosudi@gmail.com>
2017-12-18 00:46:59 +0100

    app models

--- cgi-bin/noteapp/models.py
+++ cgi-bin/noteapp/models.py
@@ -3,5 +3,13 @@ import os
 import sys
 sys.path.insert(0, '/var/www/clients/client6/web28/cgi-bin/venv/lib/python2.7/site-packages')
 
+#Import from noteapp.py
+from noteapp import db
+
 
 #import installed library
+from flask_wtf import FlaskForm 
+ 
+from wtforms import StringField, SubmitField, IntegerField, HiddenField, validators, BooleanField, PasswordField
+from wtforms.validators import Required
+
