f84a562ffb30f42d3f48f51f81eb406831428c28
Mosudi Isiaka <imosudi@gmail.com>
2017-12-18 00:44:54 +0100

    app update

---
+++ cgi-bin/noteapp/models.py
@@ -0,0 +1,7 @@
+#import virtualenv python library directory
+import os
+import sys
+sys.path.insert(0, '/var/www/clients/client6/web28/cgi-bin/venv/lib/python2.7/site-packages')
+
+
+#import installed library
