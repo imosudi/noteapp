4eb5408438c6b3bc63a5a337db4780b7d6a16acf
Mosudi Isiaka <imosudi@gmail.com>
2017-12-18 01:02:16 +0100

    app update

--- cgi-bin/noteapp/noteapp.py
+++ cgi-bin/noteapp/noteapp.py
@@ -37,6 +37,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'ap
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  
 db = SQLAlchemy(app)  
                      
+"""
+python
+from main import db
+db.create_all()
+
+"""
 
 from models import *
 
