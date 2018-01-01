35c53c146015fca91b452f0039cb35c99dccc182
Mosudi Isiaka <imosudi@gmail.com>
2018-01-01 08:56:48 +0100

    app - error pages

--- cgi-bin/noteapp/noteapp.py
+++ cgi-bin/noteapp/noteapp.py
@@ -338,13 +338,13 @@ def logout():
 
 
 
-@main.app_errorhandler(404)
+@app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('404.html', current_time=datetime.utcnow()), 404
 
-@main.app_errorhandler(500)
+@app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+    return render_template('500.html', current_time=datetime.utcnow()), 500
 
 
 
